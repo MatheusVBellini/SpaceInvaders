44cc62cf5c54cf1f6f83f3ba71a7ec548d5a7717
Matheus Violaro Bellini <matheus.bellini02@gmail.com>
2022-09-29 11:28:25 -0300

    Projectile added

---
+++ src/spaceinvaders/game_objects/Projectile.java
@@ -0,0 +1,25 @@
+package spaceinvaders.game_objects;
+
+public class Projectile extends GameObjectDynamic {
+    /*
+     * 
+     * Attributes
+     * 
+     */
+
+    /*
+     * 
+     * Methods
+     *
+     */
+    // constructor
+    public Projectile(int x, int y) {
+        super(x,y);
+    }
+
+    // update method for redrawing game objects every frame
+    @Override
+    public void update() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
