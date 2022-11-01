5daefae977feca40c4136bcdb83dd315d443c649
Matheus Violaro Bellini <matheus.bellini02@gmail.com>
2022-11-01 10:42:58 -0300

    conflict solved

--- src/spaceinvaders/game_objects/Sprite.java
+++
@@ -1,6 +0,0 @@
-package spaceinvaders.game_objects;
-
-public abstract class Sprite {
-    protected int height = GameObject.getHitboxHeight();
-    protected int width = GameObject.getHitboxWidth();
-}
